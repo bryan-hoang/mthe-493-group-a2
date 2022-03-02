f8b3d56ad687cf3c6534bd715ba2360a05cb74da
jaredmcgrath <jared@pacefactory.com>
2022-03-02 14:35:19 -0500

    Implement some more functionality (algorithm works, just edge cases remaining)

--- src/optimal.py
+++ src/optimal.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+from math import floor
 from typing import List, Union
 from statistics import StatisticsError, variance, mean
 import random
@@ -22,34 +24,113 @@ class Worker:
         self.id = id
 
     def assign(self, items: List) -> None:
+        if len(items) > self.s_max:
+            raise AssignmentError(
+                "Cannot assign {} items to worker with s_max {}".format(len(items), self.s_max))
         self.assigned_work = items
         self.num_assigned = len(items)
         self.cost = self.c * len(items)
 
+    def reassign_from(self, source: "Worker", n: int, s_min: int) -> None:
+        '''Reassigns n assigned elements from source to self'''
+        # check that source can afford to give n
+        if n > source.num_assigned - s_min:
+            template = "Cannot reassign {} elements from worker {} (s_min: {}, max reassignable: {})"
+            msg = template.format(n, source, s_min, max(
+                0, source.num_assigned - s_min))
+            raise AssignmentError(msg)
+        # check that self has capacity for n items
+        elif n + self.num_assigned > self.s_max:
+            template = "Cannot reassign {} elements to worker {} (s_max: {}, max reassignable: {})"
+            msg = template.format(n, self, self.s_max,
+                                  self.s_max - self.num_assigned)
+            raise AssignmentError(msg)
+        # check that we're reassigning non-zero items
+        elif not n > 0:
+            raise ValueError("Cannot reassign {} items".format(n))
+        # we know that source can give n items, and self has room to take n items
+        source_items = source.assigned_work
+        new_source_items = source_items[:len(source_items) - n]
+        new_self_items = self.assigned_work + \
+            source_items[len(source_items) - n:]
+        source.assign(new_source_items)
+        self.assign(new_self_items)
+
     def __repr__(self):
-        return self.__str__()
+        return self.__str__() + "\n"
 
     def __str__(self):
         template = "Worker<id: {}, s_max: {}, c: {}, num_assigned: {}, assigned_work: {}, cost: {}>"
         return template.format(self.id, self.s_max, self.c, self.num_assigned, repr(self.assigned_work), self.cost)
 
 
+InfeasibleDist = namedtuple(
+    "InfeasibleDist", "feasible excess_available excess_required")
+
+
 class InsufficientWorkersError(Exception):
-    def __init__(self, available, beta, s_min) -> None:
-        template = "Not enough employable workers (available: {}, required: {}). Increase beta ({}) and/or decrease s_min ({})"
+    def __init__(self, available: int, beta: int, s_min: int) -> None:
+        template = "Not enough employable workers (available: {}, required: {}). Decrease beta ({}) and/or s_min ({})"
         msg = template.format(available, beta, beta, s_min)
         super().__init__(msg)
 
 
+class InsufficientDataError(Exception):
+    def __init__(self, available: int, beta: int, s_min: int) -> None:
+        if available == 0:
+            msg = "No data provided. Must have at least 1 data element"
+        else:
+            template = "Not enough data to satisfy parameters (available: {}, required: {}). Decrease beta ({}) and/or s_min ({})"
+            msg = template.format(available, s_min*beta, beta, s_min)
+        super().__init__(msg)
+
+
 class InsufficientCapacityError(Exception):
-    def __init__(self, n, capacity) -> None:
+    def __init__(self, n: int, capacity: int) -> None:
         template = "Data set exceeds worker capacity (data set size: {}, capacity: {})"
         msg = template.format(n, capacity)
         super().__init__(msg)
 
 
+class InfeasibleDistributionError(Exception):
+    def __init__(self, n: int, s_min: int, infeasible_result: InfeasibleDist) -> None:
+        n_dec = infeasible_result.excess_required - infeasible_result.excess_available
+        n_inc = s_min - infeasible_result.excess_required
+        template = "No feasible distribution for workers (data set size: {}, s_min: {}).\nExcess available: {}, excess required: {}.\nIncrease n by {}, decrease n by {}, or decrease s_min"
+        msg = template.format(n, s_min, infeasible_result.excess_available,
+                              infeasible_result.excess_required, n_dec, n_inc)
+        super().__init__(msg)
+
+
+class AssignmentError(Exception):
+    def __init__(self, msg) -> None:
+        super().__init__(msg)
+
+
 def get_employable_workers(workers: List[Worker], s_min: int) -> List[Worker]:
-    return [w for w in workers if w.s_max >= s_min]
+    return [w for w in workers if w.s_max > 0 and w.s_max >= s_min]
+
+
+def check_infeasible_dist(workers: List[Worker], n: int, s_min: int):
+    # Edge case: if s_min == 0, it's always feasible
+    if s_min == 0:
+        excess_available = sum(map(lambda w: w.s_max, workers))
+        excess_required = 0
+        return InfeasibleDist(True, excess_available, excess_required)
+    # Assume feasible by default
+    feasible = True
+    # Sort workers by s_max in descending order
+    sorted_workers = sorted(workers, key=lambda w: w.s_max, reverse=True)
+    # upper bound determines how many workers we can employ to meet s_min requirements for n data points
+    upper_bound = floor(n / s_min)
+    # in the first upper_bound workers, how much excess do we have? (we can handle upper_bound * s_min to sum_{i=1}^{upper_bound}(s_max) )
+    excess_available = sum([max(0, w.s_max - s_min)
+                           for w in sorted_workers[:upper_bound]])
+    # how many slices do we require above (s_min * upper_bound) to compute n?
+    excess_required = n % s_min
+    if excess_available < excess_required:
+        feasible = False
+    return InfeasibleDist(feasible, excess_available, excess_required)
 
 
 def assign_work(workers: List[Worker], data_set: List, beta: int, s_min: int) -> List[int]:
@@ -72,36 +153,89 @@ def assign_work(workers: List[Worker], data_set: List, beta: int, s_min: int) ->
     k_employable = len(employable)
     # employable worker capacity = sum (s_max) over employable workers
     capacity = sum(map(lambda w: w.s_max, employable))
-    # CHECK 1: num employable workers >= beta
-    if k_employable < beta:
+    infeasible_dist = check_infeasible_dist(employable, n, s_min)
+    # CHECK 0: n > 0
+    if n <= 0:
+        raise InsufficientDataError(n, beta, s_min)
+    # CHECK 1: n >= beta * s_min. If s_min is 0, we pretend it's 1
+    elif n < beta * max(1, s_min):
+        raise InsufficientDataError(n, beta, s_min)
+    # CHECK 2: num employable workers >= beta
+    elif k_employable < beta:
         raise InsufficientWorkersError(k_employable, beta, s_min)
-    # TODO: Add check of n < beta * s_min before this check
-    # CHECK 2: capacity >= n
+    # CHECK 3: capacity >= n
     elif capacity < n:
         raise InsufficientCapacityError(n, capacity)
-    # TODO: Add infeasible distribution check
+    # CHECK 4: infeasible distribution
+    if not infeasible_dist.feasible:
+        raise InfeasibleDistributionError(n, s_min, infeasible_dist)
 
     # IDs of workers that receive data
     assigned_workers = []
     # index in data_set we are assigning next
     i_data = 0
-    for worker in employable:
+    last_complete_worker_idx = -1
+    while i_data < n - 1:
+        worker = employable[last_complete_worker_idx + 1]
         # how many items are remaining?
-        # TODO: need to make sure we assign at least s_min to this worker
+        # if this is < s_min (only possible for last worker), then it will be recorded by last_complete_worker_idx
         remaining = n - i_data
-        # if no items remaining, we're done
-        if remaining == 0:
-            break
         # otherwise, compute # to assign
         x = min(worker.s_max, remaining)
         # slice data_set
         items = data_set[i_data: i_data + x]
         worker.assign(items)
         # update state
+        # If we were able to assign enough data to this worker to satisfy s_min
+        if x >= s_min:
+            last_complete_worker_idx += 1
         assigned_workers.append(worker.id)
         i_data += x
 
-    # TODO: Implement the 'redistributing' algorithm to ensure len(assigned_workers) >= beta
+    # Number of workers that received > 0 work
+    num_workers_assigned = len(assigned_workers)
+    # Number of workers that received >= s_min work
+    num_complete_workers_assigned = last_complete_worker_idx + 1
+    # initialize the donor worker index
+    donor_worker_idx = last_complete_worker_idx
+
+    while num_complete_workers_assigned < max(num_workers_assigned, beta):
+        # check to make sure we still have a worker that can donate
+        if donor_worker_idx < 0:
+            # TODO: There's a bug that occurs sometimes.
+            # We need to reassign to eligible workers based on the max_num_employable
+            # print(beta, s_min)
+            # print(num_complete_workers_assigned,
+            #       num_workers_assigned, donor_worker_idx)
+            # print(employable)
+            raise AssignmentError(
+                "Unknown error occurred while attempting reassignment (ran out of workers that can donate)")
+        source = employable[donor_worker_idx]
+        # grab some reassignable data from the last complete worker
+        n_available = source.num_assigned - s_min
+        # keep reassigning from the source until it has none left to donate, and we still need to donate
+        while n_available > 0 and num_complete_workers_assigned < max(num_workers_assigned, beta):
+            # next worker we'll reassign stuff to
+            next_worker = employable[last_complete_worker_idx + 1]
+            # calculate how much we'll reassign
+            # if s_min == 0, we need to assign at least 1 element. If num_assigned > s_min already, assign 0
+            n_required = max(0, max(1, s_min) - next_worker.num_assigned)
+            n_reassign = min(n_available, n_required)
+            # perform reassignment
+            next_worker.reassign_from(source, n_reassign, s_min)
+
+            # update how much source can donate
+            n_available -= n_reassign
+            # add this worker to assigned_workers if it isn't already there
+            if not next_worker.id in assigned_workers:
+                num_workers_assigned += 1
+                assigned_workers.append(next_worker.id)
+            # check if next_worker is now complete
+            if next_worker.num_assigned >= s_min:
+                num_complete_workers_assigned += 1
+                last_complete_worker_idx += 1
+        # at this point, we've completed our reassignment or we need to move onto the next donor
+        donor_worker_idx -= 1
 
     return assigned_workers
 
@@ -261,7 +395,7 @@ def test_random(n, seed=None, verbose=0):
                 print(workers)
                 print("\nAssigned workers:")
                 print(assigned)
-        except (InsufficientWorkersError, InsufficientCapacityError) as e:
+        except (InsufficientWorkersError, InsufficientCapacityError, InfeasibleDistributionError, InsufficientDataError) as e:
             print(e)
 
 
