cc6331ef15115a7bec9a6d0be3f44e2cf5290d3c
jaredmcgrath <jared@pacefactory.com>
2022-03-29 14:18:03 -0400

    Fix restart worker script

--- scripts/restart_worker.sh
+++ scripts/restart_worker.sh
@@ -2,8 +2,10 @@
 
 LOGS="logs"
 
+# Ensure log directory exists
+[[ -d $LOGS ]] || mkdir $LOGS
+
 pkill -15 python
 cd ~/Documents/mthe-493-group-a2
 timestamp=$(date +%s)
 pipenv run python src/worker.py >"${LOGS}/${timestamp}.out.log" 2>"${LOGS}/${timestamp}.err.log" &
-
