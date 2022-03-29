7492e2d424f07c09f4a2d2d76bc9e8cec917e14e
jaredmcgrath <jared@pacefactory.com>
2022-03-29 14:13:56 -0400

    Add logs dir

--- scripts/restart_worker.sh
+++ scripts/restart_worker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+LOGS="logs"
+
 pkill -15 python
 cd ~/Documents/mthe-493-group-a2
 timestamp=$(date +%s)
-pipenv run python src/worker.py >"${timestamp}.out.log" 2>"${timestamp}.err.log" &
+pipenv run python src/worker.py >"${LOGS}/${timestamp}.out.log" 2>"${LOGS}/${timestamp}.err.log" &
 
